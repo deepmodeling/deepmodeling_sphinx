9e317f926be6e5ee56ecc2c2ade68362d574d621
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-03-03 20:57:58 -0500

    fix mobile button

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="deepmodeling_sphinx",
-    version="0.0.2",
+    version="0.0.3",
     packages=['deepmodeling_sphinx'],
     install_require=['sphinx'],
     package_data={
