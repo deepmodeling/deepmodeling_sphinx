d551c64c9f0a08451f306281a7584e34414ea518
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-05-21 02:26:31 -0400

    add docs

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+project = 'DeepModeling'
+copyright = '2022-%d, DeepModeling' % date.today().year
+author = 'DeepModeling'
+
+extensions = [
+    'deepmodeling_sphinx',
+]
+html_theme = 'sphinx_rtd_theme'
