ff994d53e4e7ca5aed98ba89f27214d28773df8f
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-08-11 02:34:55 -0400

    fix long_description issue

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+from pathlib import Path
+
+readme_file = Path(__file__).parent / "README.md"
+readme = readme_file.read_text(encoding="utf-8")
 
 setup(
     name="deepmodeling_sphinx",
@@ -18,4 +22,6 @@ setup(
                                 'banner.js',
                                 ],
     },
+    long_description=readme,
+    long_description_content_type="text/markdown",
 )
