f07416f2ee2bc6764cb6c6344926ef036d3285f0
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-07-18 03:13:16 -0400

    keep <head>

--- deepmodeling_sphinx/inject.py
+++ deepmodeling_sphinx/inject.py
@@ -121,7 +121,7 @@ def minify_html_files(app, pagename, templatename, context, doctree):
         def render(self, template, render_context):
             content = old_render(template, render_context)
             try:
-                return minify_html.minify(content, minify_js=True, minify_css=True)
+                return minify_html.minify(content, minify_js=True, minify_css=True, keep_html_and_head_opening_tags=True)
             except SyntaxError:
                 return content
 
@@ -190,7 +190,7 @@ def setup(app: Sphinx) -> Dict[str, Any]:
     app.connect("builder-inited", copy_custom_files)
     app.connect("html-page-context", insert_sidebar)
     app.connect("html-page-context", insert_icp)
-    # app.connect("html-page-context", minify_html_files)
+    app.connect("html-page-context", minify_html_files)
     app.connect("build-finished", minify_js_files)
     app.connect("build-finished", minify_css_files)
     # dark mode for rtd theme
